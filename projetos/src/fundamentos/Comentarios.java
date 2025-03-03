e1f8dc0cb7f120bf5e918e8b80de305946031a8a
Rodrigo Sabino <sr.rodrigosabino@gmail.com>
2025-03-03 10:48:39 -0300

    primeiro programa e comentários

---
+++ projetos/src/fundamentos/Comentarios.java
@@ -0,0 +1,23 @@
+package fundamentos;
+
+public class Comentarios {
+
+	public static void main(String[] args) {
+		
+		// java tem trés tipos de comentários
+		
+		// comentário de linha como esse
+		
+		/* 
+		 * comentários multilinha 
+		 */
+		
+		/**
+		 * e comentários javadoc
+		 */
+
+		// comentérios serve para documentar o código.
+		// o compilador java ignora os comentários não alterando em nada o comportamento do programa quando executado.
+		
+	}
+}
